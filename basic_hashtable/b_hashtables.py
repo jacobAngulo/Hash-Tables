f6afc69635a317f4c7dae34bb39008506605976d
Jacob Angulo <jacobangulo@hotmail.com>
2019-07-09 16:12:43 -0700

    day 2 mvp

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -4,15 +4,29 @@
 # Basic hash table key/value pair
 # '''
 class Pair:
-    def __init__(self, key, value):
-        self.key = key
+    def __init__(self, value):
         self.value = value
+        self.right = None
+
+
+class KeyBasedLinkedList:
+    def __init__(self, key, root=None):
+        self.key = key
+        self.root = root
+
+    def append(self, value):
+        current = self.root
+        while current.right is not None:
+            current = current.right
+        current.right = Pair(value)
 
 
 # '''
 # Basic hash table
 # Fill this in.  All storage values should be initialized to None
 # '''
+
+
 class BasicHashTable:
     def __init__(self, capacity=None):
         self.capacity = capacity
@@ -25,7 +39,7 @@ class BasicHashTable:
 # '''
 
 
-def hash(string, max):
+def hash(string):
     long_hash = 5381
     for char in string:
         long_hash = ((long_hash << 5) + long_hash) + ord(char)
@@ -41,33 +55,37 @@ def hash_table_insert(hash_table, key, value):
     storage = hash_table.storage
     available_index = None
     exists = False
+    key = hash(key)
     for i in range(len(storage)):
         if storage[i] == None:
             available_index = i
-        elif storage[i][0] == key:
+        elif storage[i].key == key:
             available_index = i
             exists = True
+            break
     if exists:
-        storage[i][1] = value
+        storage[available_index].append(value)
         print(f'warning: you have overwritten {key}')
-    elif available_index == None:
-        storage[len(storage)] = (key, value)
+    if available_index == None:
+        storage[len(storage)] = KeyBasedLinkedList(key, Pair(value))
         print('warning: exceeds storage space')
     else:
-        storage[available_index] = (key, value)
+        storage[available_index] = KeyBasedLinkedList(key, Pair(value))
+
+    # '''
+    # Fill this in.
+
+    # If you try to remove a value that isn't there, print a warning.
+    # '''
 
 
-# '''
-# Fill this in.
-
-# If you try to remove a value that isn't there, print a warning.
-# '''
 def hash_table_remove(hash_table, key):
     storage = hash_table.storage
     target_index = None
+    key = hash(key)
     for i in range(len(storage)):
         if storage[i] is not None:
-            if storage[i][0] == key:
+            if storage[i].key == key:
                 target_index = i
     if target_index is not None:
         storage[target_index] = None
@@ -83,12 +101,13 @@ def hash_table_remove(hash_table, key):
 def hash_table_retrieve(hash_table, key):
     storage = hash_table.storage
     target_index = None
+    key = hash(key)
     for i in range(len(storage)):
         if storage[i] is not None:
-            if storage[i][0] == key:
+            if storage[i].key == key:
                 target_index = i
     if target_index is not None:
-        return storage[i][1]
+        return storage[i].root.value
     else:
         return None
 
